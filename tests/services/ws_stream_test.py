fa24e3c3a87233a8cf805c8cdc59b3a95ecab825
twoics <second_vozhzhov@mail.ru>
2024-07-14 20:23:11 +0700

    refactor: Прогнал pre commit

--- tests/services/ws_stream_test.py
+++ tests/services/ws_stream_test.py
@@ -1,15 +1,18 @@
-import pytest
-from app.services.stream.ws import WsStream
 from json import JSONDecodeError
+
+import pytest
+
+from app.services.stream.ws import WsStream
+
 
 class TestWsStream:
     @pytest.mark.asyncio
     async def test_decode_error(self, mocker):
-        ws = mocker.AsyncMock(receive_json=mocker.AsyncMock(side_effect=JSONDecodeError(
-            msg='Test',
-            doc='',
-            pos=0
-        )))
+        ws = mocker.AsyncMock(
+            receive_json=mocker.AsyncMock(
+                side_effect=JSONDecodeError(msg='Test', doc='', pos=0)
+            )
+        )
 
         receiver = WsStream(ws)
         with pytest.raises(ValueError):
@@ -17,7 +20,9 @@ class TestWsStream:
 
     @pytest.mark.asyncio
     async def test_decode_success(self, mocker):
-        ws = mocker.AsyncMock(receive_json=mocker.AsyncMock(return_value={'hello': 'world'}))
+        ws = mocker.AsyncMock(
+            receive_json=mocker.AsyncMock(return_value={'hello': 'world'})
+        )
 
         receiver = WsStream(ws)
         data = await receiver.wait_json()
