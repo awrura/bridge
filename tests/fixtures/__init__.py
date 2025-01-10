3fb9af787b1d8bb242bdfa761d6dac4a75686a2c
twoics <second_vozhzhov@mail.ru>
2025-01-10 15:01:06 +0700

    test: Добавлены тесты для проверки прав клиента на подключение к матрице
    Написал тесты для класса AccessByJwtValidator. Попутно вынес некоторые тестовые данные в фикстуры.

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -1,4 +1,6 @@
 from .secure import jwt_parser
+from .secure import valid_jwt_claims
+from .secure import valid_userinfo
 
 # Все фикстуры импортировать сюда обязательно
-__all__ = [jwt_parser]  # pyright: ignore
+__all__ = [jwt_parser, valid_jwt_claims, valid_userinfo]  # pyright: ignore
--- tests/services/access_test.py
+++ tests/services/access_test.py
@@ -1,24 +1,15 @@
 from jwt.exceptions import PyJWTError
 from pydantic import ValidationError
 
+from app.dto.access import UserAccess
+from app.services.secure.access import AccessByJwtValidator
 from app.services.secure.access import JwtParser
 
 
 class TestJwtParser:
-    def test_success_parse(self, mocker, jwt_parser: JwtParser):
+    def test_success_parse(self, mocker, jwt_parser: JwtParser, valid_jwt_claims: dict):
         decode_mocker = mocker.patch('jwt.decode')
-        decode_mocker.return_value = {
-            'user_uuid': 'c3ec2264-36bc-4deb-9c86-92b240349643',
-            'username': 'twoics',
-            'available_matrices': [
-                {
-                    'uuid': 'a9184058-5a4e-4dc7-b94a-3968c470e281',
-                    'name': 'awrura',
-                    'height': 16,
-                    'weight': 16,
-                }
-            ],
-        }
+        decode_mocker.return_value = valid_jwt_claims
         assert jwt_parser.parse('valid-access-key') is not None
 
     def test_jwt_error(self, mocker, jwt_parser: JwtParser):
@@ -33,3 +24,19 @@ class TestJwtParser:
             ),
         )
         assert jwt_parser.parse('invalid-access-key') is None
+
+
+class TestAccessValidatior:
+    def test_access_denied(self, mocker):
+        jwt_parser = mocker.Mock(parse=mocker.Mock(return_value=None))
+        validator = AccessByJwtValidator(parser=jwt_parser)
+        assert not validator.ask_permission(
+            'invalid-access-key', 'a9184058-5a4e-4dc7-b94a-3968c470e281'
+        )
+
+    def test_access_allowed(self, mocker, valid_userinfo: UserAccess):
+        jwt_parser = mocker.Mock(parse=mocker.Mock(return_value=valid_userinfo))
+        validator = AccessByJwtValidator(parser=jwt_parser)
+        assert validator.ask_permission(
+            'valid-access-key', 'a9184058-5a4e-4dc7-b94a-3968c470e281'
+        )
